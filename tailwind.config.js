5eb9ca6bf0e469cf16afaf12721531a53bac8bc2
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-23 04:05:36 +0530

    extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
           300: "#14213D",
         },
       },
+      boxShadow: {
+        primary: "0px 6px 10px 2px rgba(0, 0, 0, 0.25)",
+        centered: "0px 0px 15px 2px rgba(0, 0, 0, 0.2)",
+      },
+      keyframes: {
+        spinreverse: {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: { spinreverse: "spinreverse 1s linear infinite" },
     },
   },
   plugins: [],
