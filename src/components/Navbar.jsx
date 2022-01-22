304cd0f8f00043903279886bd739d563a880ed7f
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-23 04:11:27 +0530

    change name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { ActiveLink } from "raviger";
 const Navbar = () => {
   return (
     <div className="bg-primary-100 flex justify-between text-3xl py-3 px-5 items-center">
-      <p>GFG Nirvana</p>
+      <p>LoveDSA</p>
       <ActiveLink
         href="/"
         className="bg bg-primary-300 text-white px-6 py-2 text-xl rounded-xl"
