9e86510d7fa1b269a190976ab2e5c7ad374a5964
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-23 14:33:32 +0530

    fix login spinner

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Login = () => {
         <Button>
           {isLoading || isFetching ? (
             <svg
-              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+              className="animate-spin h-5 w-5 text-white"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
