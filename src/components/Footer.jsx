8b03e3cabb52672a5754d160463b78b01a2c09e9
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-23 16:29:35 +0530

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
       Made with{""}
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mx-1 text-red-500"
+        className="h-5 w-5 mx-1 text-red-500"
         viewBox="0 0 20 20"
         fill="currentColor"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-          clip-rule="evenodd"
+          clipRule="evenodd"
         />
       </svg>{" "}
       by
