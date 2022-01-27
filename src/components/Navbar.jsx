61a99ff321339494dd99a8f81a781412ac281155
Shailesh Aanand <anaandshailu@gmail.com>
2022-01-27 14:33:01 +0530

    improve logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <p className="text-bold font-semibold">LoveDSA</p>
       <ActiveLink
         href="/"
-        className="bg bg-primary-300 text-white px-6 py-2 text-xl rounded-xl"
+        className="bg bg-primary-300 text-white px-5 py-2 text-xl rounded-xl flex items-center justify-center gap-2 pr-4"
         exactActiveClass="invisible"
         onClick={() => {
           localStorage.removeItem("username");
@@ -15,6 +15,20 @@ const Navbar = () => {
         }}
       >
         Logout
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+          />
+        </svg>
       </ActiveLink>
     </div>
   );
