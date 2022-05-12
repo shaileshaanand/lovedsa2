603ba353b01469e9acad849a1f8efab108ac3524
Shailesh Aanand <anaandshailu@gmail.com>
2022-05-12 09:33:19 +0530

    remove stale variable

--- src/components/TopicStatsBox.jsx
+++ src/components/TopicStatsBox.jsx
@@ -2,71 +2,8 @@ import React from "react";
 import TopicStats from "./TopicStats";
 
 const TopicStatsBox = ({ sections }) => {
-  const questions = [
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: true,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: false,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: true,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: true,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: false,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: true,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: true,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: false,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: false,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: true,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: false,
-    },
-    {
-      title: "Merge two sorted linked lists",
-      url: "https://www.geeksforgeeks.org/merge-two-sorted-linked-lists/",
-      completed: false,
-    },
-  ];
   return (
     <div className="flex flex-wrap md:flex-nowrap md:overflow-y-auto gap-8 justify-center w-full md:justify-start p-2 pb-0">
-      {/* <TopicStats questions={questions} title={"LinkedList"} /> */}
       {sections.map((section, index) => {
         return (
           <TopicStats
